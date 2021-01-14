ff271165aa9921d569254fa70a375561ddb943be
Maximov Artyom <maximov.artyom007@gmail.com>
2021-01-14 15:53:13 +0200

    Complete script (at least, I think in that way)

--- script.sh
+++ script.sh
@@ -1,1 +1,26 @@
-proxy
+#/bin/bash
+
+touch /etc/nginx/sites-available/proxy.conf
+
+cat > /etc/nginx/sites-available/proxy.conf << EOL
+
+upstream backend {
+    least_conn;
+
+    ${PROXY_HOST_1}
+    ${PROXY_HOST_2}
+    ${PROXY_HOST_3}
+    ${PROXY_HOST_4}
+    ${PROXY_HOST_5}
+  
+}
+
+server{
+  listen ${NGINX_PORT};
+  location / {
+    proxy_pass http://backend;
+  }
+}
+
+EOL
+
