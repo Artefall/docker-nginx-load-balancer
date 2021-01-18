846cc5eb5c7788363e27e8357bb37d1d13253abf
Maximov Artyom <maximov.artyom007@gmail.com>
2021-01-18 16:25:53 +0200

    Now user don't need to eneter slash symbol in PROXY_PATH

--- docker-create-proxy-script.sh
+++ docker-create-proxy-script.sh
@@ -1,44 +1,5 @@
 #!/bin/sh
 
-# set -e
-
-
-
-# mkdir /etc/nginx/sites-enabled
-# touch /etc/nginx/sites-enabled/proxy.conf
-
-# cat > /etc/nginx/sites-enabled/proxy.conf << EOL
-
-# server{
-
-#   listen ${NGINX_PORT};
-
-# EOL
-
-# for n in 1 2 3 4 5
-# do
-
-# if [[ ! -z $(printenv PROXY_PATH_${n}) ]]; then
-
-# cat >> /etc/nginx/sites-enabled/proxy.conf << EOL
-
-#   location $(printenv PROXY_PATH_${n}) {
-#     proxy_pass $(printenv PROXY_HOST_${n});
-#   }
-
-# EOL
-
-# fi;
-
-# done
-
-# echo "}" >> /etc/nginx/sites-enabled/proxy.conf;
-
-# nginx -t
-
-# # exec "$@"
-
-
 set -e
 
 
@@ -57,8 +18,8 @@ if [[ ! -z $(printenv PROXY_PATH_${n}) ]]; then
 
 cat >> /etc/nginx/conf.d/default.conf << EOL
 
-  location $(printenv PROXY_PATH_${n}) {
-    proxy_pass $(printenv PROXY_HOST_${n});
+  location /$(printenv PROXY_PATH_${n}) {
+    $(printenv PROXY_HOST_${n});
   }
 
 EOL
